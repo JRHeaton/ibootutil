8e6b3a2e7f72512454cf44197176ed78ee4a2e6b
John Heaton <jheaton.dev@gmail.com>
2010-04-15 21:24:45 -0400

    all functional except script

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <IOKit/IOCFPlugIn.h>
 #include <sys/stat.h>
 #include <readline/readline.h>
+#include <getopt.h>
 
 #define RECOVERY 0x1281
 #define DFU 0x1227
@@ -18,16 +19,23 @@
 struct iBootUSBConnection {
 	io_service_t usbService;
 	IOUSBDeviceInterface **deviceHandle;
-	CFStringRef name;
-	CFStringRef serial;
+	CFStringRef name, serial;
 	unsigned int idProduct;
 };
 typedef struct iBootUSBConnection *iBootUSBConnection;
 
+void iDevice_print(iBootUSBConnection connection) {
+	if(connection != NULL) {
+		if(connection->name && connection->serial) {
+			CFShow(connection->name);
+			CFShow(connection->serial);
+		}
+	}
+}
+
 iBootUSBConnection iDevice_open(uint32_t productID) {
 	CFMutableDictionaryRef match = IOServiceMatching(kIOUSBDeviceClassName);
 	if(match == NULL) {
-		printf("Error: couldn't create query dictionary for IOServiceGetMatchingService()\n");
 		return NULL;
 	}
 	
@@ -51,14 +59,12 @@ iBootUSBConnection iDevice_open(uint32_t productID) {
 	
 	SInt32 score;
 	if(IOCreatePlugInInterfaceForService(service, kIOUSBDeviceUserClientTypeID, kIOCFPlugInInterfaceID, &pluginInterface, &score) != 0) {
-		printf("Error: couldn't create plugin interface for device\n");
 		IOObjectRelease(service);
 		return NULL;
 	}
 	
 	if((*pluginInterface)->QueryInterface(pluginInterface, CFUUIDGetUUIDBytes(kIOUSBDeviceInterfaceID),
 										  (LPVOID*)&deviceHandle) != 0) {
-		printf("Error: couldn't create device interface\n");
 		IOObjectRelease(service);
 		return NULL;
 	}
@@ -66,7 +72,6 @@ iBootUSBConnection iDevice_open(uint32_t productID) {
 	(*pluginInterface)->Release(pluginInterface);
 	
 	if((*deviceHandle)->USBDeviceOpen(deviceHandle) != 0) { 
-		printf("Error: couldn't connect to device\n");
 		IOObjectRelease(service);
 		(*deviceHandle)->Release(deviceHandle);
 		return NULL;
@@ -87,33 +92,25 @@ iBootUSBConnection iDevice_open(uint32_t productID) {
 	connection->serial = productSerial;
 	connection->idProduct = productID;
 	
+	iDevice_print(connection);
+	
 	return connection;
 }
 
 void iDevice_close(iBootUSBConnection connection) {
 	if(connection != NULL) {
-		printf("Closing connection...\n");
-//		if(connection->usbService) IOObjectRelease(connection->usbService);
-//		if(connection->deviceHandle) (*connection->deviceHandle)->USBDeviceClose(connection->deviceHandle);
-//		if(connection->deviceHandle) (*connection->deviceHandle)->Release(connection->deviceHandle);
-//		if(connection->name) CFRelease(connection->name);
-//		if(connection->serial) CFRelease(connection->serial);
-//		
-//		free(connection);
-	}
-}
-
-void iDevice_print(iBootUSBConnection connection) {
-	if(connection != NULL) {
-		if(connection->name && connection->serial) {
-			CFShow(connection->name);
-			CFShow(connection->serial);
-		}
+		if(connection->usbService) IOObjectRelease(connection->usbService);
+		if(connection->deviceHandle) (*connection->deviceHandle)->USBDeviceClose(connection->deviceHandle);
+		if(connection->deviceHandle) (*connection->deviceHandle)->Release(connection->deviceHandle);
+		if(connection->name) CFRelease(connection->name);
+		if(connection->serial) CFRelease(connection->serial);
+		
+		free(connection);
 	}
 }
 
 int iDevice_send_command(iBootUSBConnection connection, const char *command) {
-	if(connection == NULL || command == NULL || connection->idProduct != RECOVERY)
+	if(connection == NULL || command == NULL)
 		return -1;
 	
 	IOUSBDevRequest request;
@@ -291,15 +288,166 @@ int iDevice_start_shell(iBootUSBConnection connection, const char *prompt) {
 	return 0;
 }
 
+int iDevice_run_script(const char *path) {
+	
+	return 0;
+}
+
+void usage() {
+	printf("ibootutil - iPhone USB communication tool\n");
+	printf("by Gojohnnyboi\n\n");
+	printf("Usage: ibootutil <args>\n\n");
+	
+	printf("Options:\n");
+	printf("\t-c <command>\tSend a single command\n");
+	printf("\t-f <file>\tSend a file\n");
+	printf("\t-s <script>\trun script at specified path\n");
+	printf("\t-a <addr>\tSpecify idProduct value manually\n");
+	printf("\t-r\t\tReset the usb connection\n");
+	printf("\t-p\t\tOpen a shell with iBoot\n\n");
+	
+	exit(0);
+}
+
 int main (int argc, const char **argv) {
-	iBootUSBConnection connection = iDevice_open(RECOVERY);
-	if(connection == NULL) {
-		printf("Error: couldn't find device\n");
+	if(argc < 2)
+		usage();
+	
+	iBootUSBConnection connection;
+	
+	int i, productID=0, command=0, reset=0, file=0, script=0, shell=0;
+	for(i=1;i<argc;++i) {
+		if(strcmp(argv[i], "-a") == 0) {
+			if(argv[i+1] == NULL) {
+				printf("-a requires that you specify a value\n");
+				exit(1);
+			}
+			printf("Setting idProduct to 0x%x\n", (unsigned int)strtol(argv[i+1], NULL, 16));
+			productID = strtol(argv[i+1], NULL, 16);
+		} else if(strcmp(argv[i], "-c") == 0) {
+			if(argv[i+1] == NULL) {
+				printf("-c requires that you specify a command\n");
+				exit(1);
+			}
+			command=(i+1);
+		} else if(strcmp(argv[i], "-f") == 0) {
+			if(argv[i+1] == NULL) {
+				printf("-f requires that you specify a file\n");
+				exit(1);
+			}
+			file=(i+1);
+		} else if(strcmp(argv[i], "-s") == 0) {
+			if(argv[i+1] == NULL) {
+				printf("-s requires that you provide a script path\n");
+				exit(1);
+			}
+			script=(i+1);
+		} else if(strcmp(argv[i], "-p") == 0) {
+				shell=1;
+		} else if(strcmp(argv[i], "-r") == 0) {
+			reset=1;
+		}
 	}
 	
-	iDevice_print(connection);
-	iDevice_start_shell(connection, "iDevice$ ");
-	iDevice_close(connection);
+	if(command) {
+		if(file || script || shell) {
+			printf("You can only specify one of the -cfsp options\n");
+			exit(1);
+		}
+		
+		if(!productID) {
+			productID = RECOVERY;
+		}
+		
+		connection = iDevice_open(productID);
+		if(connection == NULL) {
+			printf("Couldn't open device @ 0x%x\n", productID);
+		}
+		
+		iDevice_send_command(connection, argv[command]);
+		if(reset)
+			iDevice_reset(connection);
+		else
+			iDevice_close(connection);
+		
+		exit(0);
+	}
+	if(file) {
+		if(command || script || shell) {
+			printf("You can only specify one of the -cfsp options\n");
+			exit(1);
+		}
+		
+		if(productID) {
+			connection = iDevice_open(productID);
+			if(connection == NULL) {
+				printf("Couldn't open device @ 0x%x\n", productID);
+				exit(1);
+			}
+		} else {
+			connection = iDevice_open(RECOVERY);
+			if(connection == NULL) {
+				connection = iDevice_open(DFU);
+			}
+		}
+		if(connection == NULL) {
+			printf("Couldn't open device @ 0x%x or 0x%x\n", RECOVERY, DFU);
+			exit(1);
+		}
+		
+		if(iDevice_send_file(connection, argv[file]) != 0) {
+			printf("Couldn't send file\n");
+			iDevice_close(connection);
+			exit(1);
+		}
+		
+		if(reset)
+			iDevice_reset(connection);
+		else
+			iDevice_close(connection);
+		
+		exit(0);
+	}
+	
+	if(script) {
+		if(command || file || shell) {
+			printf("You can only specify one of the -cfsp options\n");
+			exit(1);
+		}
+		
+		if(iDevice_run_script(argv[script]) != 0) {
+			printf("Couldn't run script\n");
+			exit(1);
+		}
+		
+		exit(0);
+	}
+	
+	if(shell) {
+		if(command || file || script) {
+			printf("You can only specify one of the -cfsp options\n");
+			exit(1);
+		}
+		
+		if(!productID)
+			productID = RECOVERY;
+		
+		connection = iDevice_open(productID);
+		if(connection == NULL) {
+			printf("Couldn't open device @ 0x%x\n", productID);
+			exit(1);
+		}
+		
+		const char *prompt = "iDevice$ ";
+		
+		if(iDevice_start_shell(connection, prompt) != 0) {
+			printf("Couldn't open shell with iBoot\n");
+			exit(1);
+		}
+		
+		iDevice_close(connection);
+		exit(0);
+	}
 	
 	return 0;
 }
